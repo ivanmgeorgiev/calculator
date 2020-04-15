6cf988aa1496c5c455d511c40c3cbe8fca077d15
ivanmgeorgiev <ivan.m.georgiev@gmail.com>
2020-04-15 10:34:49 +0300

    Handle division by zero error

--- calculator.py
+++ calculator.py
@@ -39,7 +39,7 @@ class Calculator:
 
         self.subMenu = Menu(self.menu)
         self.menu.add_cascade(label='File', menu=self.subMenu)
-        self.subMenu.add_command(label='About', font=("Calibri", 10), command=lambda: messagebox.showinfo("About", "Calculator ver. 1.1 \n Ivan Georgiev"))
+        self.subMenu.add_command(label='About', font=("Calibri", 10), command=lambda: messagebox.showinfo("About", "Calculator ver. 1.11 \n Ivan Georgiev"))
         self.subMenu.add_command(label='Exit', font=("Calibri", 10), command=quit)
 
         # Configure the main window and its size
@@ -55,55 +55,55 @@ class Calculator:
         self.buttons_row0 = Frame(self.root)
         self.buttons_row0.pack(anchor=E)
         self.button_pl_mi = Button(self.buttons_row0, text=chr(177), command=self.changeSign)
-        self.button_pl_mi.pack(side=LEFT)
         self.button_sqr = Button(self.buttons_row0, text=chr(8730), command=self.calcSqrt)
-        self.button_sqr.pack(side=LEFT)
         self.button_clear = Button(self.buttons_row0, text="CE", command=self.clearText)
+        self.button_pl_mi.pack(side=LEFT)
+        self.button_sqr.pack(side=LEFT)
         self.button_clear.pack()
 
         self.buttons_row1 = Frame(self.root)
         self.buttons_row1.pack()
         self.button7 = Button(self.buttons_row1, text="7", command=lambda: changeText(7))
-        self.button7.pack(side=LEFT)
         self.button8 = Button(self.buttons_row1, text="8", command=lambda: changeText(8))
-        self.button8.pack(side=LEFT)
         self.button9 = Button(self.buttons_row1, text="9", command=lambda: changeText(9))
-        self.button9.pack(side=LEFT)
         self.button_divide = Button(self.buttons_row1, text=chr(247), command=self.calcDivide)
+        self.button7.pack(side=LEFT)
+        self.button8.pack(side=LEFT)
+        self.button9.pack(side=LEFT)
         self.button_divide.pack()
 
         self.buttons_row2 = Frame(self.root)
         self.buttons_row2.pack()
         self.buttons_row2.option_add("*side", 'LEFT')
         self.button4 = Button(self.buttons_row2, text="4", command=lambda: changeText(4))
-        self.button4.pack(side=LEFT)
         self.button5 = Button(self.buttons_row2, text="5", command=lambda: changeText(5))
-        self.button5.pack(side=LEFT)
         self.button6 = Button(self.buttons_row2, text="6", command=lambda: changeText(6))
-        self.button6.pack(side=LEFT)
         self.button_mult = Button(self.buttons_row2, text=chr(215), command=self.calcMultiply)
+        self.button4.pack(side=LEFT)
+        self.button5.pack(side=LEFT)
+        self.button6.pack(side=LEFT)
         self.button_mult.pack()
 
         self.buttons_row3 = Frame(self.root)
         self.buttons_row3.pack()
         self.button1 = Button(self.buttons_row3, text="1", command=lambda: changeText(1))
-        self.button1.pack(side=LEFT)
         self.button2 = Button(self.buttons_row3, text="2", command=lambda: changeText(2))
-        self.button2.pack(side=LEFT)
         self.button3 = Button(self.buttons_row3, text="3", command=lambda: changeText(3))
-        self.button3.pack(side=LEFT)
         self.button_minus = Button(self.buttons_row3, text="-", command=self.calcExtract)
+        self.button1.pack(side=LEFT)
+        self.button2.pack(side=LEFT)
+        self.button3.pack(side=LEFT)
         self.button_minus.pack()
 
         self.buttons_row4 = Frame(self.root)
         self.buttons_row4.pack()
         self.button0 = Button(self.buttons_row4, text="0", command=lambda: changeText(0))
-        self.button0.pack(side=LEFT)
         self.button_point = Button(self.buttons_row4, text=".", command=lambda: changeText('.'))
-        self.button_point.pack(side=LEFT)
         self.button_result = Button(self.buttons_row4, text="=", command=self.show_result)
-        self.button_result.pack(side=LEFT)
         self.button_plus = Button(self.buttons_row4, text="+", command=self.calcAdd)
+        self.button0.pack(side=LEFT)
+        self.button_point.pack(side=LEFT)
+        self.button_result.pack(side=LEFT)
         self.button_plus.pack()
 
         self.root.mainloop()
@@ -178,7 +178,7 @@ class Calculator:
             self.text.set(update)
 
     def show_result(self):
-        if self.text.get() != '+' and self.text.get() != '-' and self.text.get() != chr(215) and self.text.get() != chr(247):
+        if self.text.get() != '+' and self.text.get() != '-' and self.text.get() != chr(215) and self.text.get() != chr(247) and self.text.get() != 'Error':
             if self.operation == 'add':
                 self.result = self.num + float(self.text.get())
             elif self.operation == 'extract':
@@ -186,7 +186,10 @@ class Calculator:
             elif self.operation == 'multiply':
                 self.result = self.num * float(self.text.get())
             elif self.operation == 'divide':
-                self.result = self.num / float(self.text.get())
+                if float(self.text.get()) == 0:
+                    self.error = 1
+                else:
+                    self.result = self.num / float(self.text.get())
             elif self.operation == 'sqrt':
                 if float(self.text.get()) > 0:
                     self.result = math.sqrt(float(self.text.get()))
